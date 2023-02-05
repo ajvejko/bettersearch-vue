9e7b2a9795dc15b43344650126226e9813f1a644
ajvejko <moucka.matej01@gmail.com>
2023-02-05 16:40:48 +0100

    added a way to search

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,14 +1,32 @@
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import { buttonManagement } from "../stores/manageButton";
 const input = ref("");
+
+const search = (event) => {
+  if (event.key === "Enter") {
+    //Check if nothing is selected and input is empty
+    if (!buttonManagement.name && !input.value.trim()) {
+      return;
+    } else if (buttonManagement.name && !input.value.trim()) {
+      window.open(buttonManagement.home, "_self");
+    } else {
+      window.open(buttonManagement.search + input.value, "_self");
+    }
+  }
+};
+
+onMounted(() => {
+  document.addEventListener("keydown", search);
+});
 </script>
 
 <template>
-  <form className="mx-auto mt-20 w-1/2 font-inter">
+  <div className="mx-auto mt-20 w-1/2 font-inter">
     <!-- Input field for searching -->
     <input
       v-model="input"
+      autofocus
       placeholder="Search the web"
       class="w-full rounded-full border-2 bg-neutral-800 px-5 py-2 text-xl text-white outline-none focus:shadow-[0_0px_11px_rgba(0,0,0,0.25)] focus:shadow-blue-500"
     />
@@ -16,5 +34,5 @@ const input = ref("");
     <div class="mr-6 mt-1 text-right text-sm text-stone-800">
       {{ buttonManagement.search + input }}
     </div>
-  </form>
+  </div>
 </template>
--- src/components/WebButton.vue
+++ src/components/WebButton.vue
@@ -6,7 +6,7 @@ defineProps(["name", "home", "search"]);
 
 <template>
   <button
-    @click="$emit('toggle')"
+    @click.prevent="$emit('toggle')"
     class="mr-3 mt-4 rounded-xl border-2 border-white py-1 px-2 text-white shadow-[0_0px_8px_rgba(0,0,0,0.25)] active:shadow-blue-500"
     :class="name === buttonManagement.name ? 'border-blue-500' : ''"
   >
